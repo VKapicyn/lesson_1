017c82c3007c16054a311be668237901753b3ae9
Vladislav Kapicyn <goodsnasey@gmail.com>
2021-02-16 10:04:56 +0300

    Initial Commit

---
+++ lesson_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_1
+//
+//  Created by Владислав Капицын on 16.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
